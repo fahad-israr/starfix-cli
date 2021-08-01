71e2e4b16ebce16177d68082f84b3ecc0022c5e4
fi-javis <fahad.israr@javis.ai>
2021-08-01 12:56:37 +0530

    starfix config patch edit

--- cli/src/main/java/dev/starfix/Starfix.java
+++ cli/src/main/java/dev/starfix/Starfix.java
@@ -1,31 +1,29 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.quarkus:quarkus-jackson:1.8.1.Final
+//DEPS io.quarkus:quarkus-jackson:2.1.0.CR1
 //DEPS com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.9.2
-//DEPS io.quarkus:quarkus-picocli:1.8.1.Final
-//Q:CONFIG quarkus.banner.enabled=false
-//Q:CONFIG quarkus.log.level=WARN
+//DEPS io.quarkus:quarkus-picocli:2.1.0.CR1
+//DEPS org.zeroturnaround:zt-exec:1.12
+//FILES application.properties=../../../resources/application.properties
 
 package dev.starfix;
 import io.quarkus.runtime.annotations.RegisterForReflection;
+import org.zeroturnaround.exec.ProcessExecutor;
+import org.zeroturnaround.exec.ProcessResult;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.ExitCode;
-import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-import io.quarkus.runtime.QuarkusApplication;
+
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 import java.io.File;
+import java.util.concurrent.TimeoutException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -33,8 +31,10 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 @CommandLine.Command(mixinStandardHelpOptions = true)
 public class Starfix implements Runnable{
-    
 
+    @Parameters(arity = "0..1")
+    String uri;
+    
     @Command
     public int config() throws Exception {
         editConfig();
@@ -80,7 +80,6 @@ public class Starfix implements Runnable{
 
         } catch (Exception e) {
             e.printStackTrace();
-
         }
 
         try {
@@ -88,6 +87,7 @@ public class Starfix implements Runnable{
             String branch = "";
             if(isBlob(url))
             {   // Example URL : https://github.com/starfixdev/starfix/blob/master/cli/pom.xml
+                // Example URL2: https://github.com/hexsum/Mojo-Webqq/blob/master/script/check_dependencies.pl#L17
 
                 String temp = url.substring(url.indexOf("blob/")+5);
                 branch = temp.substring(0,temp.indexOf("/"));
@@ -120,45 +120,41 @@ public class Starfix implements Runnable{
         return ExitCode.OK;
     }
     
-    @Parameters(arity = "0..1")
-    String uri;
 
+    
     @Override
     public void run() {
+        if(uri==null||uri.isEmpty())
+        {   
+            new CommandLine(new Starfix()).usage(System.out); // Will invoke Picocli Help
+            return;
+        }
         cloneCmd(uri);
     }
 
     // Function to validate URL using with Regex
     public static boolean validate_url(String url) {
         // URL Validation to check a valid git repository
-        String pattern = "((git|ssh|http(s)?)|(git@[\\w\\.]+))(:(//)?)([\\w\\.@\\:/\\-~]+)(\\.git)?(/)?";
-        Pattern r = Pattern.compile(pattern);
-        // Now create matcher object.
-        Matcher m = r.matcher(url);
-        return m.matches();
+        String pattern = "((git|ssh|http(s)?)|(git@[\\w.]+))(:(//)?)([\\w.@:/\\-~]+)(.*)?";
+        return Pattern.matches(pattern,url);
     }
 
     // Function to check if the URL points to a file
     public static boolean isBlob(String url){
         String pattern = "^https://github.com/(.*)/blob/(.*)$";
-        Pattern r = Pattern.compile(pattern);
-        // Now create matcher object.
-        Matcher m = r.matcher(url);
-        return m.matches();
+        return Pattern.matches(pattern,url);
     }
 
     // Function yo determine if the current OS is Windows
     public static boolean isWindows() {
-        return System.getProperty("os.name").toLowerCase().indexOf("windows") >= 0;
+        return System.getProperty("os.name").toLowerCase().contains("windows");
     }
 
     // Function to fetch config file
     public static File getConfigFile() {
         String userHome = System.getProperty("user.home"); // Get User Home Directory: /home/user_name
 
-        File configFile = new File(userHome + "/.config/starfix.yaml"); // Loading YAML
-
-        return configFile;
+        return new File(userHome + "/.config/starfix.yaml");
 
     }
 
@@ -183,28 +179,35 @@ public class Starfix implements Runnable{
                 System.out.println(
                         "\n-New  configuration file will be created at: " + configFile.getAbsolutePath() + "\n");
                 configFile.createNewFile();
+            }else{
+                Config config = new ObjectMapper(new YAMLFactory()).readValue(configFile, Config.class);
+                ide = config.getIde();
+                clone_path = config.getClone_path();
             }
 
             // --------------------------First we'll input preferred IDE from
             // user------------------------------
 
             int id = 0;
-            while (id != 1 || id != 2 || id != 3) {
+            while (true) {
                 System.out.println(
                         "\n--------Chose the preferred IDE --------\n 1.for vscode \n 2.for eclipse \n 3.for IntelliJ_IDEA ");
-
-                id = Integer.parseInt(reader.readLine());
-
+                String ideInput = reader.readLine().trim();
+                if(ideInput==null || ideInput.isEmpty()){
+                    System.out.println("Empty/blank input provided - reseting to existing/default setting");
+                    break;
+                }
+                id = Integer.parseInt(ideInput);
                 if (id == 1) {
                     ide = isWindows() ? "code.cmd" : "code";
                     System.out.println("\n--------Selected IDE:VsCode--------");
                     break;
                 } else if (id == 2) {
-                    ide = "eclipse";
+                    ide = isWindows() ? "eclipse.exe":"eclipse";
                     System.out.println("\n--------Selected IDE:Eclipse--------");
                     break;
                 } else if (id == 3) {
-                    ide = "idea";
+                    ide = isWindows() ?"idea64.exe":"idea";
                     System.out.println("\n--------Selected IDE:IntelliJ_IDEA--------");
                     break;
                 } else
@@ -215,11 +218,17 @@ public class Starfix implements Runnable{
             // user--------------
             while (true) {
                 System.out.println("\n--------Enter preferred Clonning path--------");
-                clone_path = reader.readLine();
-                // We'll check if the path enterd by user is a valid path or not
-                File tmp = new File(clone_path);
-                if (tmp.exists())
+                String clonePathInput = reader.readLine();
+                if(clonePathInput == null || clonePathInput.isEmpty()){
+                    System.out.println("Empty/blank input provided - reseting to existing/default setting");
                     break;
+                }
+                // We'll check if the path enterd by user exists else try creating it
+                File tmp = new File(clonePathInput);
+                if (tmp.exists()||tmp.mkdir()){
+                    clone_path = clonePathInput;
+                    break;
+                }
                 // Incase of Invalid path he'll be shown an error and directed to try again
                 System.out.println("\n--------Invalid Path!! Try Again--------");
             }
@@ -235,12 +244,6 @@ public class Starfix implements Runnable{
 
     }
 
-    // Function to Launch the Editor
-    public static void launch_editor(Path directory, String ide, String path, String filePath) throws IOException, InterruptedException {
-        runCommand(directory.getParent(), ide, path,filePath);// Launching the editor now
-
-    }
-
     public static void gitClone(Path directory, String originUrl) throws IOException, InterruptedException {
         // Function for git clonning
         runCommand(directory.getParent(), "git", "clone", originUrl, directory.getFileName().toString());
@@ -248,71 +251,21 @@ public class Starfix implements Runnable{
 
     public static String runCommand(Path directory, String... command) throws IOException, InterruptedException {
         // Function to Run Commands using Process Builder
-        Process p = process_runner(directory, command);
-        return gobbleStream(p);
-
-    }
-
-    public static Process process_runner(Path directory, String... command) throws IOException, InterruptedException {
-        Objects.requireNonNull(directory, "directory");
-        if (!Files.exists(directory)) {
-            throw new RuntimeException("can't run command in non-existing directory '" + directory + "'");
+        ProcessResult presult;
+        try {
+            presult = new ProcessExecutor().command(command).redirectOutput(System.out).redirectErrorStream(true).readOutput(true)
+                    .execute();
+        } catch (TimeoutException e) {
+            throw new RuntimeException("Error running command", e);
         }
-        ProcessBuilder pb = new ProcessBuilder().command(command).directory(directory.toFile());
-        Process p = pb.start();
 
-        int exit = p.waitFor();
-
-        if (exit != 0) {
+        int exit = presult.getExitValue();
+        if (exit!=0) {
             throw new AssertionError(
                     String.format("runCommand %s in %s returned %d", Arrays.toString(command), directory, exit));
         }
-        return p;
-    }
 
-    public static String gobbleStream(Process p) throws IOException, InterruptedException {
-        StreamGobbler errorGobbler = new StreamGobbler(p.getErrorStream(), "E");
-        StreamGobbler outputGobbler = new StreamGobbler(p.getInputStream(), "O");
-        outputGobbler.start();
-        errorGobbler.start();
-        int exit = p.waitFor();
-        if (exit != 0) {
-            throw new AssertionError(String.format("runCommand returned %d", exit));
-        }
-        errorGobbler.join();
-        outputGobbler.join();
-        return outputGobbler.getExecResult() + errorGobbler.getExecResult();
-    }
-
-    private static class StreamGobbler extends Thread {
-        private String exec_result;
-
-        private final InputStream is;
-        private final String type;
-
-        private StreamGobbler(InputStream is, String type) {
-            this.is = is;
-            this.type = type;
-        }
-
-        @Override
-        public void run() {
-            exec_result = ""; // Resets result variable for every new process execution
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(is));) {
-                String line;
-
-                while ((line = br.readLine()) != null) {
-                    System.out.println(type + "> " + line);
-                    exec_result += line;
-                }
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-            }
-        }
-
-        public String getExecResult() {
-            return exec_result;
-        }
+        return presult.outputUTF8();
     }
 
     @RegisterForReflection
@@ -347,4 +300,38 @@ public class Starfix implements Runnable{
         }
 
     }
+
+    public static void launch_editor(Path directory, String ide, String path, String filePath) throws IOException, InterruptedException {
+
+        if(filePath.indexOf("#")>0){
+            filePath = filePath.replace("#L","#");
+            // ==== VS CODE ====
+            // code -g file:line
+            if(ide.equals("code")||ide.equals("code.cmd")){
+                filePath = filePath.replace("#",":");
+                runCommand(directory.getParent(), ide,"-g",path,filePath);
+            }
+            // ===== IntelliJ =====
+            // idea64.exe [--line <number>] [--column <number>] <path ...>
+            // idea /home/fahad/MyProjects/testings/jbang --line 3 /home/fahad/MyProjects/testings/jbang/otp.java
+            if(ide.equals("idea")||ide.equals("idea.exe")||ide.equals("idea64.exe")){
+                String lineNumber  = filePath.substring(filePath.lastIndexOf("#")+1);
+                filePath = filePath.substring(0,filePath.lastIndexOf("#"));
+                runCommand(directory.getParent(), ide,path,"--line",lineNumber,filePath);
+            }
+            // === Eclipse =====
+            // eclipse.exe file.txt:22
+            if(ide.equals("eclipse")){
+                filePath = filePath.replace("#",":");
+                runCommand(directory.getParent(), ide,path,filePath);
+            }
+
+        }
+        else{
+            runCommand(directory.getParent(),ide,path,filePath);// Launching the editor now
+        }
+    }
+
+
+
 }
